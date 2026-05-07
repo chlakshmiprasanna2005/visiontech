4d2545c61ae594f04a59a86b3fd848589652d54c
Lakshmi Prasanna <chlakshmiprasanna2005@gmail.com>
2026-05-07 17:50:29 +0530

    operators and array

---
+++ javascript/intermediate.js
@@ -0,0 +1,63 @@
+//chapter 15
+//operators
+
+//atithmetic operators -> +, -, /, *, % ,**
+
+//comparision operators
+// // == vs ===
+// let a=10;
+// let b="ram";
+// console.log(a+b);
+
+// console.log(a==b);
+// console.log(a===b);
+
+// let x=10;
+// let y="30";
+
+// //number+string
+// //string operator
+// console.log(x+y);//1030
+// console.log(y-x);//30-10
+
+//Arrays
+//thre are 2 ways of creating an array
+//1.
+
+// let storeroom=["table","chairs", true, false, 50];
+
+// //2. constructor way
+// let score= new Array(2,3,4,5);
+// console.log(score);
+
+
+// console.log(storeroom.push("hat"));
+// console.log(storeroom);
+
+// console.log(storeroom.pop());
+// console.log(storeroom);
+
+
+// console.log(storeroom.unshift("hat"));
+// console.log(storeroom);
+
+// console.log(storeroom.shift());
+// console.log(storeroom);
+
+// storeroom[3]="ram";
+// console.log(storeroom);
+
+// let newarray=["hello" ,"world"]
+// console.log(newarray.join(" "));
+
+
+let a=[1,2,3];
+let b=["ram","sita"];
+console.log(a.concat(b));
+
+
+
+
+
+
+
