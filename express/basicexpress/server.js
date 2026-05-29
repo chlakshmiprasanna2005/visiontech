b6a8162651461244198450ef57be4b67fd3ee716
Lakshmi Prasanna <chlakshmiprasanna2005@gmail.com>
2026-05-29 17:35:57 +0530

    express server

---
+++ express/basicexpress/server.js
@@ -0,0 +1,33 @@
+// common js module
+// const express = require("express")
+
+import express, { request, response } from 'express';
+
+const server = express()
+
+// HTTP methods - get, post, push, delete
+// we also use put and patch 
+
+// routes 
+
+// API- end points(or own api- like dummy data ):
+
+server.get('/', (request,response)=>{
+    response.send("<h1>hello from server</h1>");
+})
+
+server.get('/about',(request, response)=>{
+    response.send("<h1>about page</h1>");
+})
+server.get('/services',(request, response)=>{
+    response.send("<h1>services page</h1>");
+})
+
+server.get('/api',(request, response)=>{
+    response.json({"name":"Lakshmi"});
+})
+
+server.listen(4000, ()=>{
+    console.log("server running on port 4000");
+    
+})
--- nodejs/new1.js
+++ nodejs/new1.js
@@ -1,11 +1,14 @@
 //2 ways of modules export and import- common JS snd ES6
 //comon JS-old
-const {add,sub, mul}= require("./new.js")
+// const {add,sub, mul}= require("./new.js")
+
+// console.log(add(3,4));
+// console.log(sub(3,4));
+// console.log(mul(3,4));
+
 
 //ES module-new
 // import add from './new.js'
 
-console.log(add(3,4));
-console.log(sub(3,4));
-console.log(mul(3,4));
+
 
